408f1b29b637b012fa601816a26883c3cb5edd23
unknown <tomerpacific@gmail.com>
2024-12-13 15:57:22 +0200

    CarIdentifier initial project commit

---
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tomerpacific.caridentifier
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.tomerpacific.caridentifier.ui.theme.CarIdentifierTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CarIdentifierTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
