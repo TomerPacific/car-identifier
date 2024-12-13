9244a6dcbebef84fd5d8274de9aa1c4528d27c0e
unknown <tomerpacific@gmail.com>
2024-12-13 19:53:07 +0200

    CarIdentifier removing unused import

--- app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tomerpacific.caridentifier
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
