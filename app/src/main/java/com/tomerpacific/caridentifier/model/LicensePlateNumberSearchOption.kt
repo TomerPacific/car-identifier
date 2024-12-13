b7f297ae237889b3c080a7b70670bdc106f7eba9
unknown <tomerpacific@gmail.com>
2024-12-13 22:44:57 +0200

    CarIdentifier creating viewmodel and enum to represent user's choice of searching for license plate

---
+++ app/src/main/java/com/tomerpacific/caridentifier/model/LicensePlateNumberSearchOption.kt
@@ -0,0 +1,5 @@
+package com.tomerpacific.caridentifier.model
+
+enum class LicensePlateNumberSearchOption {
+    IMAGE, TEXT
+}
---
+++ app/src/main/java/com/tomerpacific/caridentifier/model/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.tomerpacific.caridentifier.model
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    fun handleClickOnSearchOption(searchOption: LicensePlateNumberSearchOption) {
+
+    }
+
+}
