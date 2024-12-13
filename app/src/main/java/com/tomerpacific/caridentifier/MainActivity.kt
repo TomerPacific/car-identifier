0825457227d54113d6d2aba6b39951e7ca8e45c6
unknown <tomerpacific@gmail.com>
2024-12-13 19:34:30 +0200

    CarIdentifier adding title

--- app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -3,10 +3,19 @@ package com.tomerpacific.caridentifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.tomerpacific.caridentifier.ui.theme.CarIdentifierTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +27,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Column(modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Top) {
+                        Row(modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center) {
+                            Text("Car Identifier",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 25.sp,
+                                textAlign = TextAlign.Center)
+                        }
+                    }
                 }
             }
         }
