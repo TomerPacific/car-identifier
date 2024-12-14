68666d200bf10f985699268792b723a8f331f2f0
unknown <tomerpacific@gmail.com>
2024-12-14 19:57:31 +0200

    CarIdentifier adding state for showing/dismissing dialog

--- app/src/main/java/com/tomerpacific/caridentifier/model/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/model/MainViewModel.kt
@@ -1,11 +1,19 @@
 package com.tomerpacific.caridentifier.model
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel: ViewModel() {
 
-    fun handleClickOnSearchOption(searchOption: LicensePlateNumberSearchOption) {
+    private val _shouldDisplayDialogToTypeLicenseNumber = MutableStateFlow(false)
+    val shouldDisplayDialogToTypeLicenseNumber: StateFlow<Boolean>
+        get() = _shouldDisplayDialogToTypeLicenseNumber
 
+    fun handleClickOnSearchOption(searchOption: LicensePlateNumberSearchOption) {
+        if (searchOption == LicensePlateNumberSearchOption.TEXT) {
+            _shouldDisplayDialogToTypeLicenseNumber.value = true
+        }
     }
 
 }
