1a465ebe7b4a8ce183206c6c7a7b19583d2f2482
unknown <tomerpacific@gmail.com>
2024-12-14 19:57:59 +0200

    CarIdentifier creating composable function for dialog

--- app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -11,13 +11,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,9 +43,13 @@ import com.tomerpacific.caridentifier.ui.theme.CarIdentifierTheme
 class MainActivity : ComponentActivity() {
 
     private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val shouldDisplayDialogToTypeLicensePlate = mainViewModel.shouldDisplayDialogToTypeLicenseNumber.collectAsState()
+
             CarIdentifierTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -65,8 +85,75 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
+                    if (shouldDisplayDialogToTypeLicensePlate.value) {
+                        LicensePlateNumberDialog()
+                    }
                 }
             }
         }
+    }
+
+
+    @Composable
+    fun LicensePlateNumberDialog() {
+
+        val focusRequester = remember {
+            FocusRequester()
+        }
+
+        val licensePlateNumber = remember {
+            mutableStateOf("")
+        }
+
+        AlertDialog(
+            onDismissRequest = {
+
+            },
+            text = {
+                Column {
+                    Text("Enter License Plate Number", fontWeight = FontWeight.Bold)
+                    Spacer(Modifier.height(10.dp))
+                    OutlinedTextField(
+                        modifier = Modifier.focusRequester(focusRequester),
+                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                        value = licensePlateNumber.value,
+                        onValueChange = {
+                            licensePlateNumber.value = it
+                        },
+                        placeholder = {
+                            Text("License Plate Number")
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Blue,
+                            unfocusedBorderColor = Color.Black,
+                        ),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                }
+
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+
+                    },
+                    enabled = licensePlateNumber.value.isNotEmpty()
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
     }
 }
