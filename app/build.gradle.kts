16f1c4b9462e00e9db21675fc739fa273c8ca96d
unknown <tomerpacific@gmail.com>
2024-12-16 23:11:55 +0200

    CarIdentifier adding ktor dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     implementation("androidx.compose.material3:material3-android:1.3.1")
+    implementation("io.ktor:ktor-client-core:3.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
