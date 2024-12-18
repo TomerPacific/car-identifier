98286eea22d9483c3fe6c23ceef17e7d51f2ae69
unknown <tomerpacific@gmail.com>
2024-12-18 17:46:32 +0200

    CarIdentifier adding configuration for serializing json

--- app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
@@ -2,7 +2,13 @@ package com.tomerpacific.caridentifier.data
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 
 val AppHttpClient: HttpClient by lazy {
-    HttpClient(CIO)
+    HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
 }
