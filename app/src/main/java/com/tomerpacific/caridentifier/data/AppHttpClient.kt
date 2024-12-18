bb230fbf36b9b18547ad357031a175b4f6b364c7
unknown <tomerpacific@gmail.com>
2024-12-18 11:07:13 +0200

    CarIdentifier adding CIO to client

--- app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
@@ -1,7 +1,8 @@
 package com.tomerpacific.caridentifier.data
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 
 val AppHttpClient: HttpClient by lazy {
-    HttpClient()
+    HttpClient(CIO)
 }
