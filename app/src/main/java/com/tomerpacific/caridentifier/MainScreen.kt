b10db6d48d387bc1287318e5427dfd57a3b82ee2
unknown <tomerpacific@gmail.com>
2024-12-19 11:05:46 +0200

    CarIdentifier creating composable for main screen

---
+++ app/src/main/java/com/tomerpacific/caridentifier/MainScreen.kt
@@ -0,0 +1,81 @@
+package com.tomerpacific.caridentifier
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.tomerpacific.caridentifier.model.MainViewModel
+import com.tomerpacific.caridentifier.ui.theme.CarIdentifierTheme
+
+@Composable
+fun MainScreen(navController: NavController) {
+
+    val mainViewModel: MainViewModel = viewModel()
+
+    val shouldDisplayDialogToTypeLicensePlate = mainViewModel
+        .shouldDisplayDialogToTypeLicenseNumber
+        .collectAsState()
+
+    CarIdentifierTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center) {
+                    Text("Car Identifier",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp,
+                        textAlign = TextAlign.Center)
+                }
+                Spacer(modifier = Modifier.size(200.dp))
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        CarLicensePlateSearchOptionButton(
+                            buttonText = "חפש לפי תמונה",
+                            drawableId = R.drawable.license_plate,
+                            drawableContentDescription = "License Plate",
+                            navController
+                        )
+                        CarLicensePlateSearchOptionButton(
+                            buttonText = "חפש לי מספר",
+                            drawableId = R.drawable.keyboard,
+                            drawableContentDescription = "Smartphone Keyboard",
+                            navController
+                        )
+                    }
+                }
+            }
+            if (shouldDisplayDialogToTypeLicensePlate.value) {
+                LicensePlateNumberDialog()
+            }
+        }
+    }
+}
