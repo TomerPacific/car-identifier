cab7e4824b5edd80a0df5eaa66702e5e53f364ae
unknown <tomerpacific@gmail.com>
2024-12-20 17:13:48 +0200

    CarIdentifier instantiating viewmodel and passing it to composables in navgraph

--- app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -3,16 +3,20 @@ package com.tomerpacific.caridentifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.dialog
 import androidx.navigation.compose.rememberNavController
+import com.tomerpacific.caridentifier.model.MainViewModel
 import com.tomerpacific.caridentifier.model.Screen
 
 class MainActivity : ComponentActivity() {
 
+
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,8 +30,8 @@ class MainActivity : ComponentActivity() {
             composable(route = Screen.MainScreen.route) {
                 MainScreen(navController = navController)
             }
-            dialog(route = Screen.LicensePlateNumberInput.route) { LicensePlateNumberDialog(navController)}
-            composable(route = Screen.CarDetailsScreen.route) { CarDetailsScreen()}
+            dialog(route = Screen.LicensePlateNumberInput.route) { LicensePlateNumberDialog(navController, mainViewModel)}
+            composable(route = Screen.CarDetailsScreen.route) { CarDetailsScreen(mainViewModel)}
         }
     }
 }
