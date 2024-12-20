780238c7dc4cb1828cc36db2bdbf01000f369e5e
unknown <tomerpacific@gmail.com>
2024-12-20 11:08:11 +0200

    CarIdentifier adding route for new screen

--- app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
@@ -3,4 +3,5 @@ package com.tomerpacific.caridentifier.model
 sealed class Screen(val route: String) {
     object MainScreen : Screen("main")
     object LicensePlateNumberInput : Screen("license_plate_number_input")
+    object CarDetailsScreen : Screen("car_details_screen")
 }
