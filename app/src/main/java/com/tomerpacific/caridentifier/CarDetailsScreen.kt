bf072c2ecc72b481fa048530dca684630e61fa27
unknown <tomerpacific@gmail.com>
2024-12-21 12:18:33 +0200

    CarIdentifier refactoring UI of car details screen. Adding method to convert car manufacturer to English

--- app/src/main/java/com/tomerpacific/caridentifier/CarDetailsScreen.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/CarDetailsScreen.kt
@@ -15,37 +15,89 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tomerpacific.caridentifier.model.CarDetails
 import com.tomerpacific.caridentifier.model.MainViewModel
 
 @Composable
 fun CarDetailsScreen(mainViewModel: MainViewModel) {
 
-     val carDetails = mainViewModel.carDetails.collectAsState()
+    val carDetails = mainViewModel.carDetails.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-
-        if (carDetails.value == null) {
-            CircularProgressIndicator(
-                modifier = Modifier.width(64.dp),
-                color = MaterialTheme.colorScheme.secondary,
-                trackColor = MaterialTheme.colorScheme.surfaceVariant,
-            )
-        } else {
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center) {
-                Text("Car Details")
-            }
-            Spacer(modifier = Modifier.height(100.dp))
-            Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center) {
-                Text(carDetails.value?.commercialName!!)
-            }
-        }
-
-
+    val arrangement: Arrangement.Vertical = when (carDetails.value) {
+        null -> Arrangement.Center
+        else -> Arrangement.Top
     }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = arrangement
+    ) {
 
+        when (carDetails.value) {
+            null -> Spinner()
+            else -> CarInformation(carDetails.value!!)
+        }
+    }
 }
+
+@Composable
+fun Spinner() {
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+    )
+}
+
+@Composable
+fun CarInformation(details: CarDetails) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text("פרטי הרכב", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+    }
+    Spacer(modifier = Modifier.height(20.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text("${getCarManufacturer(details.manufacturerName)} ${details.commercialName.lowercase().replaceFirstChar { it.titlecase() }} ${details.trimLevel.lowercase().replaceFirstChar { it.titlecase() }} ${details.yearOfProduction}",
+            fontSize = 20.sp)
+    }
+    Spacer(modifier = Modifier.height(100.dp))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(" טסט אחרון בוצע בתאריך: ${details.lastTestDate}", fontSize = 20.sp)
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(" בעלות נוכחית: ${details.ownership}", fontSize = 20.sp)
+    }
+}
+
+private fun getCarManufacturer(manufacturer: String): String {
+    return when (manufacturer) {
+        "טויוטה" -> "Toyota"
+        "סובארו" -> "Subaru"
+        "סוזוקי" -> "Suzuki"
+        "סיאט" -> "Seat"
+        "סאאב" -> "Saab"
+        "סאנגיונג" -> "Sangyong"
+        "פורד" -> "Ford"
+        "פיאט" -> "Fiat"
+        "פולקסווגן" -> "Volkswagen"
+        "פיג'ו" -> "Peugeot"
+        "פונטיאק" -> "Pontiac"
+        "פורשה" -> "Porsche"
+        else -> "Unknown Manufacturer"
+    }
+}
+
