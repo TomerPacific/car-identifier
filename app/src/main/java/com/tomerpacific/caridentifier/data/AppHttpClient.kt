5cc6d1a62ad4556a4ce92fe78fe637ddae5fce7b
unknown <tomerpacific@gmail.com>
2024-12-23 20:29:58 +0200

    CarIdentifier using the Android engine instead of CIO one

--- app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/AppHttpClient.kt
@@ -1,12 +1,12 @@
 package com.tomerpacific.caridentifier.data
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 
 val AppHttpClient: HttpClient by lazy {
-    HttpClient(CIO) {
+    HttpClient(Android) {
         install(ContentNegotiation) {
             json()
         }
