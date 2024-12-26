139b44bd24278c3a6b7a285212feedeb86d6ad95
unknown <tomerpacific@gmail.com>
2024-12-26 20:14:08 +0200

    CarIdentifier refactoring composable to accept composable as argument

--- app/src/main/java/com/tomerpacific/caridentifier/CarDetailWithIcon.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/CarDetailWithIcon.kt
@@ -2,16 +2,12 @@ package com.tomerpacific.caridentifier
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.foundation.text.appendInlineContent
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
 import androidx.compose.ui.text.buildAnnotatedString
@@ -20,8 +16,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun CarDetailWithIcon(iconId: String,
                       text: String,
-                      iconResourceId: Int,
-                      contentDescription: String) {
+                      icon: @Composable () -> Unit) {
 
     val annotatedString = buildAnnotatedString {
         append(text)
@@ -38,10 +33,7 @@ fun CarDetailWithIcon(iconId: String,
                     placeholderVerticalAlign = PlaceholderVerticalAlign.Center
                 )
             ) {
-                Icon(
-                    painterResource(id = iconResourceId), contentDescription,
-                    tint = Color(254, 219, 0),
-                    modifier = Modifier.fillMaxSize())
+                icon()
             }
         )
     )
