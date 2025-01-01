4d8df5b02ce71eb0f5b0631fa2cebfe1c9b352c7
unknown <tomerpacific@gmail.com>
2025-01-01 19:04:00 +0200

    CarIdentifier adding route for camera permission composable

--- app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
@@ -4,4 +4,5 @@ sealed class Screen(val route: String) {
     object MainScreen : Screen("main")
     object LicensePlateNumberInput : Screen("license_plate_number_input")
     object CarDetailsScreen : Screen("car_details_screen")
+    object CameraPermission: Screen("camera_permission")
 }
