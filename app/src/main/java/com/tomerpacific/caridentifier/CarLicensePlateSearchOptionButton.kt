5ea342e0aac9404a7ed1dbc47c4a7ae3f46d1c3e
unknown <tomerpacific@gmail.com>
2025-01-01 21:24:40 +0200

    CarIdentifier logic to color image based on camera permission state and to disable click

--- app/src/main/java/com/tomerpacific/caridentifier/CarLicensePlateSearchOptionButton.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/CarLicensePlateSearchOptionButton.kt
@@ -4,16 +4,20 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -25,16 +29,26 @@ import com.tomerpacific.caridentifier.model.Screen
 fun CarLicensePlateSearchOptionButton(buttonText: String,
                                       drawableId: Int,
                                       drawableContentDescription: String,
-                                      navController: NavController) {
+                                      navController: NavController,
+                                      isCameraPermissionGranted:Boolean? = false) {
 
     Column(
-        modifier = Modifier.clickable {
-            val navigationRoute = when (drawableId) {
-                R.drawable.license_plate -> Screen.CameraPermission.route
-                else -> Screen.LicensePlateNumberInput.route
-            }
-            navController.navigate(route = navigationRoute)
-        },
+        modifier = Modifier.then(
+            if (isCameraPermissionGranted != null && !isCameraPermissionGranted) {
+                Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = {}
+                )
+            } else {
+                Modifier.clickable {
+                    val navigationRoute = when (drawableId) {
+                        R.drawable.license_plate -> Screen.CameraPermission.route
+                        else -> Screen.LicensePlateNumberInput.route
+                    }
+                    navController.navigate(route = navigationRoute)
+                }
+            }),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -48,8 +62,16 @@ fun CarLicensePlateSearchOptionButton(buttonText: String,
                 .clip(CircleShape),
             painter = painterResource(drawableId),
             contentDescription = drawableContentDescription,
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            colorFilter = decideOnColorFilter(drawableId, isCameraPermissionGranted)
         )
         Text(buttonText)
     }
+}
+
+private fun decideOnColorFilter(drawableId: Int, isCameraPermissionGranted: Boolean?): ColorFilter? {
+    if (drawableId == R.drawable.license_plate && isCameraPermissionGranted != null && !isCameraPermissionGranted) {
+        return ColorFilter.colorMatrix(ColorMatrix().apply { setToSaturation(0f) })
+    }
+    return null
 }
