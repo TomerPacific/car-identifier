cf3dec523ec6beca6620ddd61ee7176b6a8b6111
unknown <tomerpacific@gmail.com>
2025-01-01 21:24:58 +0200

    CarIdentifier updating permission state based on user action

--- app/src/main/java/com/tomerpacific/caridentifier/HandleCameraPermission.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/HandleCameraPermission.kt
@@ -1,17 +1,24 @@
 package com.tomerpacific.caridentifier
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.pm.PackageManager
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import com.tomerpacific.caridentifier.model.MainViewModel
 
 @Composable
-fun HandleCameraPermission(navController: NavController) {
+fun HandleCameraPermission(navController: NavController,
+                           mainViewModel: MainViewModel) {
 
     val context = LocalContext.current
 
@@ -27,18 +34,30 @@ fun HandleCameraPermission(navController: NavController) {
 
         }
         PackageManager.PERMISSION_DENIED -> {
+            val activity = context.getActivityOrNull()
+            activity?.let {
+                if (shouldShowRequestPermissionRationale(it, android.Manifest.permission.CAMERA)) {
+                    Toast.makeText(context, "Camera permission is needed to scan license plate", Toast.LENGTH_LONG).show()
+                } else {
+                    navController.popBackStack()
+                }
+            }
+
             val cameraPermissionRequestLauncher = rememberLauncherForActivityResult(
                 ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
                     cameraPermissionStatus = when(isGranted) {
                         true -> PackageManager.PERMISSION_GRANTED
                         false -> PackageManager.PERMISSION_DENIED
                     }
+
+                mainViewModel.setCameraPermissionState(isGranted)
                     when(isGranted) {
                         true -> {
-        
+
                         }
                         false -> {
                             navController.popBackStack()
+
                         }
 
                     }
@@ -50,4 +69,14 @@ fun HandleCameraPermission(navController: NavController) {
         }
     }
 
+}
+
+fun Context.getActivityOrNull(): Activity? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+
+    return null
 }
