9f126fe611e498cb032a1f13b1b30486c1830b83
unknown <tomerpacific@gmail.com>
2025-01-03 16:20:21 +0200

    CarIdentifier handling different types of responses

--- app/src/main/java/com/tomerpacific/caridentifier/CarDetailsScreen.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/CarDetailsScreen.kt
@@ -21,7 +21,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tomerpacific.caridentifier.model.ApiResponse
 import com.tomerpacific.caridentifier.model.CarDetails
+import com.tomerpacific.caridentifier.model.FailureResponse
 import com.tomerpacific.caridentifier.model.MainViewModel
 
 @Composable
@@ -56,56 +58,59 @@ fun Spinner() {
 }
 
 @Composable
-fun CarInformation(details: CarDetails) {
+fun CarInformation(details: ApiResponse) {
 
-    if (details.licensePlateNumber == 0) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("לא ניתן להשיג את פרטי הרכב. נסו שנית.", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+    when (details) {
+        is FailureResponse -> {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("לא ניתן להשיג את פרטי הרכב. נסו שנית.", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+            }
         }
-    } else {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("פרטי הרכב", fontSize = 25.sp, fontWeight = FontWeight.Bold)
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("${getCarManufacturer(details.manufacturerName)} ${details.commercialName.lowercase().replaceFirstChar { it.titlecase() }} ${details.trimLevel.lowercase().replaceFirstChar { it.titlecase() }} ${details.yearOfProduction}",
-                fontSize = 20.sp)
-        }
-        Spacer(modifier = Modifier.height(100.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(" טסט אחרון בוצע בתאריך: ${details.lastTestDate}", fontSize = 20.sp)
-        }
+        is CarDetails -> {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("פרטי הרכב", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("${getCarManufacturer(details.manufacturerName)} ${details.commercialName.lowercase().replaceFirstChar { it.titlecase() }} ${details.trimLevel.lowercase().replaceFirstChar { it.titlecase() }} ${details.yearOfProduction}",
+                    fontSize = 20.sp)
+            }
+            Spacer(modifier = Modifier.height(100.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(" טסט אחרון בוצע בתאריך: ${details.lastTestDate}", fontSize = 20.sp)
+            }
 
-        CarDetailWithIcon(
-            iconId = "keysIcon",
-            text = " בעלות נוכחית: ${details.ownership} "
-        ) {
-            Icon(painterResource(id = R.drawable.ic_key_icon),"Key Icon",
-                tint = Color(254, 219, 0),
-                modifier = Modifier.fillMaxSize())
-        }
+            CarDetailWithIcon(
+                iconId = "keysIcon",
+                text = " בעלות נוכחית: ${details.ownership} "
+            ) {
+                Icon(painterResource(id = R.drawable.ic_key_icon),"Key Icon",
+                    tint = Color(254, 219, 0),
+                    modifier = Modifier.fillMaxSize())
+            }
 
-        CarDetailWithIcon(
-            iconId = "fuelIcon",
-            text = " סוג דלק: ${details.fuelType} "
-        ) {
-            Icon(
-                painterResource(id = R.drawable.ic_fuel_type), "Fuel Icon",
-                tint = Color(18, 80, 255),
-                modifier = Modifier.fillMaxSize()
-            )
+            CarDetailWithIcon(
+                iconId = "fuelIcon",
+                text = " סוג דלק: ${details.fuelType} "
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.ic_fuel_type), "Fuel Icon",
+                    tint = Color(18, 80, 255),
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 }
