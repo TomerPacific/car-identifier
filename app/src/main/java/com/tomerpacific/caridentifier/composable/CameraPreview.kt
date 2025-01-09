b66019994fa81e34f8699a2743fb7377b8689c0a
unknown <tomerpacific@gmail.com>
2025-01-09 22:11:09 +0200

    CarIdentifier refactoring removing unused code and imports

--- app/src/main/java/com/tomerpacific/caridentifier/composable/CameraPreview.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/composable/CameraPreview.kt
@@ -1,15 +1,8 @@
 package com.tomerpacific.caridentifier.composable
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.net.Uri
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
+import android.util.Log
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
@@ -19,7 +12,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,14 +20,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.tomerpacific.caridentifier.CameraFileUtils.takePicture
 import java.util.concurrent.Executors
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 @Composable
 fun CameraPreview(navController: NavController) {
@@ -45,7 +34,6 @@ fun CameraPreview(navController: NavController) {
     val executor = remember { Executors.newSingleThreadExecutor() }
     val capturedImageUri = remember { mutableStateOf<Uri?>(null) }
 
-    //val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
     val cameraController: LifecycleCameraController = remember {
         LifecycleCameraController(context).apply {
             bindToLifecycle(lifecycleOwner)
@@ -64,18 +52,17 @@ fun CameraPreview(navController: NavController) {
             }
         )
         Button(onClick = {
-            //captureImage(cameraController, context, navController)
             takePicture(cameraController, context, executor, { uri ->
                 capturedImageUri.value = uri
             }, { imageCaptureException ->
-                
+                Log.e("CameraPreview", "Error capturing image", imageCaptureException)
+                navController.popBackStack()
             })
             }) {
             Text(text = "Capture Image")
         }
         capturedImageUri.value?.let { uri ->
             Image(
-                // Asynchronously loads and displays the image from the URI
                 painter = rememberAsyncImagePainter(uri),
                 contentDescription = null,
                 modifier = Modifier
@@ -85,34 +72,4 @@ fun CameraPreview(navController: NavController) {
             )
         }
     }
-}
-
-private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
-    suspendCoroutine { continuation ->
-        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
-            cameraProvider.addListener({
-                continuation.resume(cameraProvider.get())
-            }, ContextCompat.getMainExecutor(this))
-    }
-}
-
-private fun captureImage(cameraController: LifecycleCameraController,
-                         context: Context, navController: NavController) {
-    cameraController.takePicture(
-        ContextCompat.getMainExecutor(context),
-        object : ImageCapture.OnImageCapturedCallback() {
-            override fun onCaptureSuccess(image: ImageProxy) {
-                var bitmap = image.toBitmap()
-                val matrix = Matrix().apply {
-                    postRotate(image.imageInfo.rotationDegrees.toFloat() * -1)
-                    postScale(-1f, -1f)
-                }
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-                image.close()
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                navController.popBackStack()
-            }
-        })
 }
