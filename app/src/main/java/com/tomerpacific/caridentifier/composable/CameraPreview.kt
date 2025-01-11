658dadae7e6fb889b7199b63d2c752fc5b228e9c
unknown <tomerpacific@gmail.com>
2025-01-11 15:08:51 +0200

    CarIdentifier styling take picture button

--- app/src/main/java/com/tomerpacific/caridentifier/composable/CameraPreview.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/composable/CameraPreview.kt
@@ -7,18 +7,23 @@ import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
 import com.tomerpacific.caridentifier.CameraFileUtils.takePicture
+import com.tomerpacific.caridentifier.R
 import com.tomerpacific.caridentifier.model.Screen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -58,8 +63,10 @@ fun CameraPreview(navController: NavController) {
                 Log.e("CameraPreview", "Error capturing image", imageCaptureException)
                 navController.popBackStack()
             })
-            }) {
-            Text(text = "Capture Image")
+            }, modifier = Modifier.padding(bottom = 15.dp)) {
+            Icon(painterResource(R.drawable.ic_camera),
+                contentDescription = "Capture Image",
+                modifier = Modifier.size(50.dp))
         }
     }
 }
