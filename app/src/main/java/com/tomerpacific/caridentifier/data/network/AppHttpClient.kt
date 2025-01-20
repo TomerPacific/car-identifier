3f0474e1aad7a6d0d3341a634b1964411f850669
unknown <tomerpacific@gmail.com>
2025-01-20 23:30:32 +0200

    CarIdentifier replacing http client

--- app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
@@ -1,19 +1,31 @@
 package com.tomerpacific.caridentifier.data.network
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
+import okhttp3.CertificatePinner
 
 val AppHttpClient: HttpClient by lazy {
-    HttpClient(Android) {
+    HttpClient(OkHttp) {
+        engine {
+            preconfigured = okhttp3.OkHttpClient.Builder()
+                .certificatePinner(
+                CertificatePinner.Builder()
+                    .add("car-license-number-fetcher.onrender.com",
+                        "sha256/mEflZT5enoR1FuXLgYYGqnVEoZvmf9c2bVBpiOjYQ0c=")
+                    .build())
+                .build()
+        }
         install(ContentNegotiation) {
             json(
-                Json {
+                contentType = Json,
+                json = kotlinx.serialization.json.Json {
                     ignoreUnknownKeys = true
                 }
             )
         }
     }
+
 }
