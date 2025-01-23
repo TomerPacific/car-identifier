f4d637a41652aa4ff8bcfb990ecff635c02a5351
unknown <tomerpacific@gmail.com>
2025-01-23 18:27:55 +0200

    CarIdentifier removing certificate pinning logic

--- app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
@@ -5,17 +5,12 @@ import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.kotlinx.json.json
-import okhttp3.CertificatePinner
 
 val AppHttpClient: HttpClient by lazy {
     HttpClient(OkHttp) {
+
         engine {
             preconfigured = okhttp3.OkHttpClient.Builder()
-                .certificatePinner(
-                CertificatePinner.Builder()
-                    .add("car-license-number-fetcher.onrender.com",
-                        "sha256/mEflZT5enoR1FuXLgYYGqnVEoZvmf9c2bVBpiOjYQ0c=")
-                    .build())
                 .build()
         }
         install(ContentNegotiation) {
