3324cc8240da96000e275811aab3a1f578c73738
unknown <tomerpacific@gmail.com>
2025-01-28 22:40:20 +0200

    CarIdentifier passing adding method to get car review

--- app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarDetailsRepository.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarDetailsRepository.kt
@@ -3,4 +3,6 @@ package com.tomerpacific.caridentifier.data.repository
 class CarDetailsRepository(private val carLicensePlateSource: CarLicensePlateSource = CarLicensePlateSource()) {
     suspend fun getCarDetails(licensePlateNumber: String) = carLicensePlateSource.getCarDetails(licensePlateNumber)
 
+    suspend fun getCarReview(searchQuery: String) = carLicensePlateSource.getCarReview(searchQuery)
+
 }
