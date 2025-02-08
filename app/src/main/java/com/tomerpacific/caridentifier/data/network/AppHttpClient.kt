3d7e2396f0c454bc7079b39fcc5791a30e7ee868
unknown <tomerpacific@gmail.com>
2025-02-08 17:49:33 +0200

    CarIdentifier adding request timeout

--- app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/network/AppHttpClient.kt
@@ -2,12 +2,16 @@ package com.tomerpacific.caridentifier.data.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 val AppHttpClient: HttpClient by lazy {
     HttpClient(Android) {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 10000
+        }
         install(ContentNegotiation) {
             json(
                 Json {
