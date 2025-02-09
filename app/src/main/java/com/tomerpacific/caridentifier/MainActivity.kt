139f591e01e7e0e10bd098b324882f1821f65484
unknown <tomerpacific@gmail.com>
2025-02-09 19:42:16 +0200

    CarIdentifier passing view model as argument to route

--- app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
             dialog(route = Screen.LicensePlateNumberInput.route) { LicensePlateNumberDialog(navController, mainViewModel) }
             composable(route = Screen.CarDetailsScreen.route) { CarDetailsScreen(mainViewModel, navController) }
             dialog(route = Screen.CameraPermission.route) { HandleCameraPermission(navController, mainViewModel) }
-            composable(route = Screen.CameraPreview.route) { CameraPreview(navController) }
+            composable(route = Screen.CameraPreview.route) { CameraPreview(navController, mainViewModel) }
             dialog(route = Screen.VerifyPhoto.route + "/{imageUri}", arguments = listOf(
                 navArgument(IMAGE_URI_KEY) {
                     type = NavType.StringType
