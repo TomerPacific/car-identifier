cfc217cfb35d1c6d311bd4e248a2c6b5959cc3a0
unknown <tomerpacific@gmail.com>
2025-02-17 19:42:57 +0200

    CarIdentifier renaming test method names

--- app/src/test/java/com/tomerpacific/caridentifier/UtilitiesUnitTest.kt
+++ app/src/test/java/com/tomerpacific/caridentifier/UtilitiesUnitTest.kt
@@ -9,24 +9,24 @@ class UtilitiesUnitTest {
     private val validLicensePlateNumber = "17-655-76"
 
     @Test
-    fun getCarManufacturer_correct() {
+    fun `should return true if translated car manufacturer name is Nissan`() {
         val translatedCarManufacturer = getCarManufacturer(carManufacturerNissan)
         assert(translatedCarManufacturer == "Nissan")
     }
 
     @Test
-    fun getCarManufacturer_unknown() {
+    fun `should return true if translated car manufacturer name is Unknown`() {
         val translatedCarManufacturer = getCarManufacturer(carManufacturerUnknown)
         assert(translatedCarManufacturer == "Unknown Manufacturer")
     }
 
     @Test
-    fun isLicensePlateValid_correct() {
+    fun `should return true if license plate is valid`() {
         assert(isLicensePlateNumberValid(validLicensePlateNumber))
     }
 
     @Test
-    fun isLicensePlateValid_incorrect() {
+    fun `should return false if license plate is not valid`() {
         assert(!isLicensePlateNumberValid(""))
     }
 }
