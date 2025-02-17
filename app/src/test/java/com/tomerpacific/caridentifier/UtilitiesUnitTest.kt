59b8da0096f0f325ff082024e98abd571cef1af4
unknown <tomerpacific@gmail.com>
2025-02-17 19:58:53 +0200

    CarIdentifier adding unit test for concatenating car make and model

--- app/src/test/java/com/tomerpacific/caridentifier/UtilitiesUnitTest.kt
+++ app/src/test/java/com/tomerpacific/caridentifier/UtilitiesUnitTest.kt
@@ -1,5 +1,6 @@
 package com.tomerpacific.caridentifier
 
+import com.tomerpacific.caridentifier.model.CarDetails
 import org.junit.Test
 
 class UtilitiesUnitTest {
@@ -33,5 +34,31 @@ class UtilitiesUnitTest {
     @Test
     fun `should return true when license plate is valid amd matches pattern`() {
         assert(isLicensePlateNumberValid(validLicensePlateNumber, Regex("^[0-9]{2,3}-[0-9]{2,3}-[0-9]{2,3}")))
+    }
+
+    @Test
+    fun `should return true when car make and model are concatenated correctly`() {
+        val carDetails = CarDetails(
+            1765576,
+            "גרמניה",
+            "SPORT",
+            0,
+            15,
+            2013,
+            "2024-03-21",
+            "2025-04-30",
+            "פרטי",
+            "WF0KXXGCBKDU75517",
+            "כחול מטלי",
+            "215/55R16",
+            "215/55R16",
+            "בנזין",
+            "2013-5",
+            "FOCUS",
+            "פורד",
+            )
+
+        val concatenatedCarMakeAndModel = concatenateCarMakeAndModel(carDetails)
+        assert(concatenatedCarMakeAndModel == "Ford Focus Sport 2013")
     }
 }
