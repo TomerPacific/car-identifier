914a9703e2e57cc5b83a0b5689af827b6e2062bc
unknown <tomerpacific@gmail.com>
2025-03-02 20:23:38 +0200

    CarIdentifier refactoring class to be able to update state

--- app/src/main/java/com/tomerpacific/caridentifier/network/AndroidConnectivityObserver.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/network/AndroidConnectivityObserver.kt
@@ -4,47 +4,47 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.core.content.getSystemService
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 
 class AndroidConnectivityObserver(
     context: Context
-): ConnectivityObserver {
+) {
 
     private val connectivityManager = context.getSystemService<ConnectivityManager>()
-    override val isConnected: Flow<Boolean>
-        get() = callbackFlow {
 
-            val callback = object: ConnectivityManager.NetworkCallback() {
+    val isConnected : MutableState<Boolean> = mutableStateOf(false)
 
-                override fun onAvailable(network: Network) {
-                    trySend(true)
-                }
+    private val callback = object: ConnectivityManager.NetworkCallback() {
 
-                override fun onLost(network: Network) {
-                    trySend(false)
-                }
-
-                override fun onUnavailable() {
-                    trySend(false)
-                }
-
-                override fun onCapabilitiesChanged(
-                    network: Network,
-                    networkCapabilities: NetworkCapabilities
-                ) {
-                    super.onCapabilitiesChanged(network, networkCapabilities)
-                    val connected: Boolean = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-                    trySend(connected)
-                }
-            }
-
-            connectivityManager?.registerDefaultNetworkCallback(callback)
-
-            awaitClose {
-                connectivityManager?.unregisterNetworkCallback(callback)
-            }
+        override fun onAvailable(network: Network) {
+            isConnected.value = true
         }
+
+        override fun onLost(network: Network) {
+            isConnected.value = false
+        }
+
+        override fun onUnavailable() {
+            isConnected.value = false
+        }
+
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+            val connected: Boolean = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            isConnected.value = connected
+        }
+    }
+
+    init {
+        connectivityManager?.registerDefaultNetworkCallback(callback)
+    }
+
+    fun unregisterNetworkCallback() {
+        connectivityManager?.unregisterNetworkCallback(callback)
+    }
 }
