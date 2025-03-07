620b8a2d2a9cce44827b43608832f702e5417012
unknown <tomerpacific@gmail.com>
2025-03-07 17:01:33 +0200

    CarIdentifier changing boolean to mutable state

--- app/src/main/java/com/tomerpacific/caridentifier/LanguageTranslator.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/LanguageTranslator.kt
@@ -5,12 +5,16 @@ import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.tasks.await
 
 class LanguageTranslator {
 
     private val englishHebrewTranslator: Translator
-    private var didDownloadLanguageModel: Boolean = false
+
+    private val _didDownloadLanguageModel = MutableStateFlow(false)
+    val didDownloadLanguageModel: StateFlow<Boolean> get() = _didDownloadLanguageModel
 
     init {
         val options = TranslatorOptions.Builder()
@@ -24,17 +28,21 @@ class LanguageTranslator {
             .build()
         englishHebrewTranslator.downloadModelIfNeeded(conditions)
             .addOnSuccessListener {
-                didDownloadLanguageModel = true
+                _didDownloadLanguageModel.value = true
             }
     }
 
     suspend fun translate(text: String): Result<String> {
 
-        if (!didDownloadLanguageModel) {
+        if (!_didDownloadLanguageModel.value) {
             return Result.failure(Exception("Failed to download language model"))
         }
 
-        val translatedText = englishHebrewTranslator.translate(text).await()
-        return Result.success(translatedText)
+        return try {
+            val translatedText = englishHebrewTranslator.translate(text).await()
+            Result.success(translatedText)
+        } catch (e: Exception) {
+            Result.failure(Exception("Translation failed: ${e.message}", e))
+        }
     }
 }
