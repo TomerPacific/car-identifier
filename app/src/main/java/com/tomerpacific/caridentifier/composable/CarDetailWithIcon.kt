64a4e4aea5f99c3fff64ba304415373904128527
unknown <tomerpacific@gmail.com>
2025-06-27 00:27:02 +0300

    CarIdentifier adding UI to allow for tooltips

--- app/src/main/java/com/tomerpacific/caridentifier/composable/CarDetailWithIcon.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/composable/CarDetailWithIcon.kt
@@ -1,25 +1,41 @@
 package com.tomerpacific.caridentifier.composable
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarDetailWithIcon(iconId: String,
                       labelText: String,
                       content: String,
+                      tooltipDescription: String,
                       icon: @Composable () -> Unit) {
 
     val annotatedString = buildAnnotatedString {
@@ -51,9 +67,31 @@ fun CarDetailWithIcon(iconId: String,
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ) {
-        Text(annotatedString,
-            inlineContent = inlineTextContent,
-            fontSize = 20.sp)
+        TooltipBox(
+            positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+            tooltip = {
+                PlainTooltip {
+                    Column(modifier = Modifier
+                        .padding(8.dp)
+                        .clip(
+                            RoundedCornerShape(16.dp)
+                        )
+                        .background(Color.White)
+                        .padding(8.dp)) {
+                        icon()
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(tooltipDescription, color = Color.Black, fontSize = 20.sp, lineHeight = 28.sp)
+                    }
+
+                }
+              },
+            state = rememberTooltipState()
+        ) {
+            Text(annotatedString,
+                inlineContent = inlineTextContent,
+                fontSize = 20.sp)
+        }
+
     }
 
 
