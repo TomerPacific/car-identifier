d20ee50c73c2b2f82721caf9b09a6dbeca4605d6
unknown <tomerpacific@gmail.com>
2025-07-20 22:50:51 +0300

    CarIdentifier adding field to state

--- app/src/main/java/com/tomerpacific/caridentifier/data/MainUiState.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/MainUiState.kt
@@ -1,10 +1,12 @@
 package com.tomerpacific.caridentifier.data
 
 import com.tomerpacific.caridentifier.model.CarDetails
+import com.tomerpacific.caridentifier.model.CarReview
 
 data class MainUiState(
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
     val carDetails: CarDetails? = null,
+    val carReview: CarReview? = null,
     val reviewUrl: String? = null
 )
