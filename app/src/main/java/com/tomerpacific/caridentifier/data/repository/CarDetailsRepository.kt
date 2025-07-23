0c3ddd91f8960c82cc91f015c4c6a335e407d60b
unknown <tomerpacific@gmail.com>
2025-07-23 22:49:15 +0300

    CarIdentifier lint fixes

--- app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarDetailsRepository.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarDetailsRepository.kt
@@ -3,6 +3,7 @@ package com.tomerpacific.caridentifier.data.repository
 class CarDetailsRepository(private val carLicensePlateSource: CarLicensePlateSource = CarLicensePlateSource()) {
     suspend fun getCarDetails(licensePlateNumber: String) = carLicensePlateSource.getCarDetails(licensePlateNumber)
 
-    suspend fun getCarReview(searchQuery: String, locale: String) = carLicensePlateSource.getCarReview(searchQuery, locale)
+    suspend fun getCarReview(searchQuery: String, locale: String) =
+        carLicensePlateSource.getCarReview(searchQuery, locale)
 
 }
