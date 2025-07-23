9446c4d70cba3fcce31f2e6b18bf5fbd0c8f527f
unknown <tomerpacific@gmail.com>
2025-07-23 22:35:59 +0300

    CarIdentifier adding detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,26 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    parallel = true
+    buildUponDefaultConfig = true
+
+    input = files(
+        "$projectDir/app/src/main/java",
+        "$projectDir/app/src/main/kotlin"
+    )
+
+    config = files("$projectDir/config/detekt/detekt.yml")
+    baseline = file("$projectDir/config/detekt/baseline.xml")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+    }
 }
