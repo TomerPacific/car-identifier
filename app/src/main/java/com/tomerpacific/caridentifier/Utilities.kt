e667789d74c424b2bb6302c13466d1f119cf29b5
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-17 20:51:01 +0200

    CarIdentifier adding KDoc

--- app/src/main/java/com/tomerpacific/caridentifier/Utilities.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/Utilities.kt
@@ -195,6 +195,16 @@ fun isLicensePlateNumberValid(licensePlateNumber: String): Boolean {
     return licensePlateNumberPatterns.any { pattern -> pattern.matches(licensePlateNumber) }
 }
 
+/**
+ * Extracts a license plate number from the provided ML Kit Text object.
+ *
+ * This function sanitizes the text from each line by replacing colons with dashes and removing spaces.
+ * It then finds all lines that match the predefined license plate patterns and returns the one
+ * with the highest OCR confidence score.
+ *
+ * @param text The ML Kit Text object from which to extract the license plate number.
+ * @return The license plate number with the highest confidence, or null if none is found.
+ */
 fun getLicensePlateNumberFromImageText(text: Text): String? {
     return text.textBlocks
         .flatMap { it.lines }
