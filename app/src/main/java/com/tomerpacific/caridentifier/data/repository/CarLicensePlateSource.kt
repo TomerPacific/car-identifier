8c1267b84195d035a56b642e833c3ac1e4669c34
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-24 20:04:23 +0200

    CarIdentifier refactoring http client creation

--- app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarLicensePlateSource.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/data/repository/CarLicensePlateSource.kt
@@ -1,5 +1,6 @@
 package com.tomerpacific.caridentifier.data.repository
 
+import com.tomerpacific.caridentifier.BuildConfig
 import com.tomerpacific.caridentifier.HEBREW_LANGUAGE_CODE
 import com.tomerpacific.caridentifier.data.network.AppHttpClient
 import com.tomerpacific.caridentifier.model.CarDetails
@@ -10,12 +11,10 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.URLProtocol
 import io.ktor.http.encodedPath
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-private const val ENDPOINT = "car-license-number-fetcher.onrender.com"
 private const val HTTP_STATUS_OK_LOWER_LIMIT = 200
 private const val HTTP_STATUS_OK_UPPER_LIMIT = 299
 
@@ -23,10 +22,8 @@ class CarLicensePlateSource(private val client: HttpClient = AppHttpClient) {
     private suspend fun HttpClient.getCarDetails(licensePlateNumber: String): Result<CarDetails> {
         val httpResponse: HttpResponse =
             try {
-                get {
+                get(BuildConfig.BASE_URL) {
                     url {
-                        protocol = URLProtocol.HTTPS
-                        host = ENDPOINT
                         encodedPath = "/vehicle/$licensePlateNumber"
                     }
                 }
@@ -48,10 +45,8 @@ class CarLicensePlateSource(private val client: HttpClient = AppHttpClient) {
     private suspend fun HttpClient.getTirePressure(licensePlateNumber: String): Result<TirePressure> {
         val httpResponse: HttpResponse =
             try {
-                get {
+                get(BuildConfig.BASE_URL) {
                     url {
-                        protocol = URLProtocol.HTTPS
-                        host = ENDPOINT
                         encodedPath = "/tire-pressure/$licensePlateNumber"
                     }
                 }
@@ -77,10 +72,8 @@ class CarLicensePlateSource(private val client: HttpClient = AppHttpClient) {
     ): Result<String> {
         val httpResponse: HttpResponse =
             try {
-                get {
+                get(BuildConfig.BASE_URL) {
                     url {
-                        protocol = URLProtocol.HTTPS
-                        host = ENDPOINT
                         encodedPath = "/review/$searchQuery"
                     }
                     header("Accept-Language", locale)
