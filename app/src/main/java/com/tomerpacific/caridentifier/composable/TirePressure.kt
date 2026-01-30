abe64c223cf185a7a1ec66001e57e5a550a2ee19
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-01-30 22:39:48 +0200

    CarIdentifier refactoring UI of screen

--- app/src/main/java/com/tomerpacific/caridentifier/composable/TirePressure.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/composable/TirePressure.kt
@@ -1,24 +1,28 @@
 package com.tomerpacific.caridentifier.composable
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -34,58 +38,61 @@ import com.tomerpacific.caridentifier.model.allPropertiesNull
 @Composable
 fun TirePressure(viewModel: MainViewModel) {
     val mainUiState by viewModel.mainUiState.collectAsState()
-    val tirePressure = mainUiState.tirePressure
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = stringResource(id = R.string.tire_pressure),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
-        )
         when {
             mainUiState.isLoading -> {
                 Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
                     LoaderAnimation(R.raw.loading_tire_animation)
                 }
             }
-            tirePressure != null -> {
-                if (tirePressure.allPropertiesNull()) {
+            mainUiState.tirePressure != null -> {
+                if (mainUiState.tirePressure!!.allPropertiesNull()) {
                     Text(text = stringResource(id = R.string.no_tire_pressure_data_for_car_error_msg))
                 } else {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        tirePressure.frontPsi?.let {
-                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.tire_pressure),
-                                    contentDescription = "Front Tire"
-                                )
-                                Text(text = "Front PSI: $it", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                            }
-                        }
-                        tirePressure.rearPsi?.let {
-                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.ic_tire_pressure),
-                                    contentDescription = "Rear Tire"
-                                )
-                                Text(text = "Rear PSI: $it", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                            }
-                        }
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center) {
+                        Text(
+                            text = stringResource(id = R.string.tire_pressure),
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
+                        )
                     }
-                    tirePressure.note?.let {
-                        Text(text = it, fontSize = 16.sp, modifier = Modifier.padding(top = 10.dp))
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center) {
+                        Image(
+                            modifier =
+                                Modifier
+                                    .size(250.dp)
+                                    .border(
+                                        BorderStroke(1.dp, Color.Black),
+                                        CircleShape,
+                                    )
+                                    .clip(CircleShape),
+                            painter = painterResource(id = R.drawable.tire_pressure),
+                            contentDescription = "Front Tire"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center) {
+
+                        mainUiState.tirePressure!!.frontPsi?.let {
+                            Text(text = "Front Tire PSI: $it", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                        }
+                        mainUiState.tirePressure!!.rearPsi?.let {
+                            Text(text = "Rear Tire PSI: $it", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                        }
                     }
                 }
             }
+
             mainUiState.errorMessage != null -> {
                 Spacer(modifier = Modifier.size(20.dp))
                 Text(
@@ -101,13 +108,6 @@ fun TirePressure(viewModel: MainViewModel) {
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                 )
-                if (viewModel.shouldShowRetryRequestButton()) {
-                    IconButton(onClick = {
-                        viewModel.getTirePressure()
-                    }) {
-                        Icon(imageVector = Icons.Default.Refresh, contentDescription = "Retry")
-                    }
-                }
             }
         }
     }
