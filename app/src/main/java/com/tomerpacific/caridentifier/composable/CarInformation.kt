1b853fa42ee2d9e7762b372c5e28be9ec184b6fd
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-18 19:25:57 +0200

    CarIdentifier reverted last commit

--- app/src/main/java/com/tomerpacific/caridentifier/composable/CarInformation.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/composable/CarInformation.kt
@@ -21,9 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,9 +43,7 @@ import com.tomerpacific.caridentifier.model.CarViewModel
 @Composable
 fun Details(carViewModel: CarViewModel) {
     val uiState by carViewModel.mainUiState.collectAsState()
-    val shouldShowRetryButton by remember(uiState) {
-        derivedStateOf { carViewModel.shouldShowRetryRequestButton.value }
-    }
+    val shouldShowRetryButton by carViewModel.shouldShowRetryRequestButton.collectAsState()
 
     val columnVerticalArrangement: Arrangement.Vertical =
         when (uiState.carDetails) {
