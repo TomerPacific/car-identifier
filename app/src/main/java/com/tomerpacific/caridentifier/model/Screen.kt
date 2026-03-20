c882d13f9297d6515cf88a70cb3d2bcfdd0b3f52
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-03-20 10:57:26 +0200

    CarIdentifier moving imageUri to route

--- app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
+++ app/src/main/java/com/tomerpacific/caridentifier/model/Screen.kt
@@ -1,5 +1,9 @@
 package com.tomerpacific.caridentifier.model
 
+import android.net.Uri
+
+const val IMAGE_URI_KEY = "imageUri"
+
 sealed class Screen(val route: String) {
     object MainScreen : Screen("main")
 
@@ -13,5 +17,7 @@ sealed class Screen(val route: String) {
 
     object GalleryPicker : Screen("gallery_picker")
 
-    object VerifyPhoto : Screen("verify_photo")
+    object VerifyPhoto : Screen("verify_photo/{$IMAGE_URI_KEY}") {
+        fun createRoute(uri: String) = "verify_photo/${Uri.encode(uri)}"
+    }
 }
